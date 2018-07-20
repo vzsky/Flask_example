986068ebf3d60172cb34e266b3c8232b453506a0
VZSKY <talay@10ninox.com>
2018-07-20 16:46:53 +0700

    test

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def sent(id):
 
 
 def get_remote_ip():
-    return request.remote_addr
+    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 
 @app.route('/port', methods=['GET' , 'POST'])
 def portcheck():
